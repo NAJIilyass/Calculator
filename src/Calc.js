c6e7c2782015bb88f0d0b9d888378f2f997619d7
IlYess <najiilyassoo@gmail.com>
2024-02-08 14:08:19 +0100

    Initialize variables

--- src/Calc.js
+++ src/Calc.js
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 const Calc = () => {
+    const ch1 = ["(", ")", "ans", "del", "clear"];
+    const ch2 = [7, 8, 9, "%", "√"];
+    const ch3 = [4, 5, 6, "x", "÷"];
+    const ch4 = [1, 2, 3, "+", "-"];
+    const ch5 = [".", 0, "±", "ENTER"];
+    const [value, setValue] = useState("0");
+    const [line, setLine] = useState("0");
+    const [firstZero, setFirstZero] = useState(false);
+    const [ans, setAns] = useState("0");
+    const [ans1, setAns1] = useState("0"); //For ans button
+    const [click, setClick] = useState(false); //Si value est vide, number s'écrit directement dans value
+    const [clickNumber, setClickNumber] = useState(false); //Je fait dans le code le fait que if(value===ans) pour traiter le cas quand user change le symbole (entre + et - par exemple), clickNumber pour le cas de si user a créer sur un nombre egal à ans
+    const [syntaxeError, setSyntaxeError] = useState(false);
+    const [clickParenthesis, setClickParenthesis] = useState(0); //Array for couting value where we click ")", the first is the number and the second is the index
+
     return <div>Calc</div>;
 };
 
